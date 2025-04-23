6b1162c1b9db3bc732c403ca74c9f81b04bb158c
Samuel1722003 <131738910+Samuel1722003@users.noreply.github.com>
2025-04-22 17:45:59 -0700

    Versión mas actualizada

--- hfe_frontend/lib/main.dart
+++ hfe_frontend/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:hfe_frontend/routes/app_routes.dart'; // ← Asegúrate de importar tu AppRoute
+import 'package:hfe_frontend/routes/app_routes.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://dmvgtocktnzamzaqvybr.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtdmd0b2NrdG56YW16YXF2eWJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQwMzU5ODMsImV4cCI6MjA1OTYxMTk4M30.D2CRrHdpdtprSRBbrCsaP26wDt5z1NS5GsjRkt1UxqE',
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -13,12 +18,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'HFE Frontend',
-      initialRoute: AppRoute.initialRoute,// ← La ruta inicial, la cual es 'home'
-      routes: AppRoute.getMenuRoutes(),    // ← Aquí van todas las rutas del menú
-      onGenerateRoute: AppRoute.onGenerateRoute, // ← Si se va por una ruta que no existe
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      initialRoute:
+          AppRoute.initialRoute, // ← La ruta inicial, la cual es 'home'
+      routes: AppRoute.getMenuRoutes(), // ← Aquí van todas las rutas del menú
+      onGenerateRoute:
+          AppRoute.onGenerateRoute, // ← Si se va por una ruta que no existe
+      theme: ThemeData(primarySwatch: Colors.blue),
     );
   }
 }
