6f07603552d3e0d4f92e6eab9b72adfde65395cd
Samuel1722003 <131738910+Samuel1722003@users.noreply.github.com>
2025-04-25 15:46:58 -0700

    Ya sirve el inicio de sesión

--- hfe_frontend/lib/widgets/sign_up_input_text.dart
+++ hfe_frontend/lib/widgets/sign_up_input_text.dart
@@ -4,18 +4,42 @@ class SignUpInputText extends StatelessWidget {
   final String hintText;
   final bool obscureText;
   final IconData? prefixIcon;
+  final TextEditingController? controller;
+  final TextInputType keyboardType;
+  final String? Function(String?)? validator;
+  final void Function(String)? onChanged;
+  final FocusNode? focusNode;
+  final TextInputAction? textInputAction;
+  final void Function()? onEditingComplete;
+  final void Function(String)? onSubmitted;
 
   const SignUpInputText({
     super.key,
     required this.hintText,
     this.obscureText = false,
     this.prefixIcon,
+    this.controller,
+    this.keyboardType = TextInputType.text,
+    this.validator,
+    this.onChanged,
+    this.focusNode,
+    this.textInputAction,
+    this.onEditingComplete,
+    this.onSubmitted,
   });
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      controller: controller,
       obscureText: obscureText,
+      keyboardType: keyboardType,
+      focusNode: focusNode,
+      textInputAction: textInputAction,
+      onEditingComplete: onEditingComplete,
+      onFieldSubmitted: onSubmitted,
+      onChanged: onChanged,
+      validator: validator,
       decoration: InputDecoration(
         hintText: hintText,
         prefixIcon: prefixIcon != null ? Icon(prefixIcon, color: Colors.grey) : null,
@@ -24,6 +48,14 @@ class SignUpInputText extends StatelessWidget {
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(5),
           borderSide: BorderSide.none,
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: const BorderSide(color: Colors.red, width: 1.0),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: const BorderSide(color: Colors.red, width: 2.0),
         ),
       ),
     );
