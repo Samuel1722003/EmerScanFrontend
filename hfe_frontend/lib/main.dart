524e635fa4310e478d0478e60d7932b3fea7d64b
Samuel1722003 <131738910+Samuel1722003@users.noreply.github.com>
2025-05-05 11:38:18 -0700

    mejora en la seguridad

--- hfe_frontend/lib/main.dart
+++ hfe_frontend/lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hfe_frontend/routes/app_routes.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
+  // Cargar las variables de entorno antes de inicializar Supabase
+  await dotenv.load();
+  
   await Supabase.initialize(
-    url: 'https://dmvgtocktnzamzaqvybr.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtdmd0b2NrdG56YW16YXF2eWJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQwMzU5ODMsImV4cCI6MjA1OTYxMTk4M30.D2CRrHdpdtprSRBbrCsaP26wDt5z1NS5GsjRkt1UxqE',
+    url: dotenv.env['SUPABASE_URL'] ?? '',
+    anonKey: dotenv.env['SUPABASE_ANON_KEY'] ?? '',
   );
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -19,11 +22,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'EmerScan',
-      initialRoute:
-          AppRoute.initialRoute, // ← La ruta inicial, la cual es 'home'
-      routes: AppRoute.getMenuRoutes(), // ← Aquí van todas las rutas del menú
-      onGenerateRoute:
-          AppRoute.onGenerateRoute, // ← Si se va por una ruta que no existe
+      initialRoute: AppRoute.initialRoute,
+      routes: AppRoute.getMenuRoutes(),
+      onGenerateRoute: AppRoute.onGenerateRoute,
       theme: ThemeData(primarySwatch: Colors.blue),
     );
   }
